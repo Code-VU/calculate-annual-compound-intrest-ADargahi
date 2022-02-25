369c3919c355ae81697776cc98e8cdc434efd334
Abbas Dargahi <adargahi97@gmail.com>
2022-02-25 11:06:42 -0600

    Assignment 4.5.3

--- compoundInterest.py
+++ compoundInterest.py
@@ -1,14 +1,24 @@
 def calculateCompoundInterest():
-    
-# This first 3 lines are provided for yougetACompoundIntrest()
-# This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
- #print("Compound Interest: "+str(intrest))
+    count = 0
+    for i in range(0, 3):
+        count += 1
+        # This first 3 lines are provided for you
+        client_one_principal = float(input("Principle (amount): "))
+        client_one_time =      float(input("Time:               "))
+        client_one_rate =      float(input("Rate:               "))
+
+        amount = client_one_principal * (1.0 + client_one_rate/100.0)**client_one_time
+        compInt = amount - client_one_principal
+
+        print("Compound Interest: "+str("{:.2f}".format(compInt)))
+
+        if count == 3:
+            break
+        else:
+            print("---")
+
 
     # end assignment
-
 ## if you want to test locally before you try to sync
 ## uncomment calculateCompoundInterest() and run > python monkeyCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
